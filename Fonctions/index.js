ddaec36fdfee189a8afba78f8edcff40c328df1d
Nenesu <ineshamid@gmail.com>
2021-02-10 14:17:37 +0100

    Update index.js

--- Fonctions/index.js
+++ Fonctions/index.js
@@ -4,14 +4,14 @@ const token = require('../Configs/token.json'); //le token est stocké à part p
 
 
 bot.on("ready", async() => {
-    console.log("bot allumé"); //un message dans la console pour être sûr que tout marche
+    console.log("bot allume"); //un message dans la console pour être sûr que tout marche
     bot.user.setStatus("online");
     bot.user.setActivity("*help", { type: "LISTENING" }); //sans type : "joue à", ici : "écoute ..."
 }); //Quand le bot est ready, on lance la fonction async qui affiche le statut du bot "en train de jouer..." et en online
 
 
 bot.on("guildMemberAdd", member => {
-    bot.channels.cache.get('807173186373419038') //on envoie le message de bienvenue du bot dans un salon spécifique via son ID
+    bot.channels.cache.get('807173186373419038').send('Bienvenue $USER !'); //on envoie le message de bienvenue du bot dans un salon spécifique via son ID
 });
 
 bot.login(token.token); //lance le bot
