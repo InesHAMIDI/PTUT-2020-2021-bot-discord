9f6e1210ba3bb6614476dc9a40a5fe7df9c26bcb
InesHAMIDI <ineshamid@gmail.com>
2021-02-17 15:08:13 +0100

    still working on welcome message...

--- Fonctions/index.js
+++ Fonctions/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'); //typique #include<>
 const bot = new Discord.Client(); //prérequis pour  créer le bot
 const token = require('../Configs/token.json'); //le token est stocké à part pour simplifier
-
+const prefix = require('../Configs/config.json'); //on stocke le préfix des commandes dans une variable pour rendre les fonctions plus pratiques à implémenter
 
 bot.on("ready", async() => {
     console.log("bot allume"); //un message dans la console pour être sûr que tout marche
@@ -10,29 +10,22 @@ bot.on("ready", async() => {
 }); //Quand le bot est ready, on lance la fonction async qui affiche le statut du bot "en train de jouer..." et en online
 
 bot.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'général');
-    // On veut envoyer un message de bienvenue dans un salon spécial par rapport à son nom pour qu'il s'adapte à tous les serveurs, si le channel n'existe pas, on arrête
+    //if (member.user.bot) return; //on ne considère pas les autres bots comme des utilisateurs
+    const channel = member.guild.channels.fetch("807173186373419038");
+    console.log(channel);
     if (!channel) return;
     channel.send(`Bienvenue ${member}`);
-    member.roles.remove('membre');
 }); //fonction donnée comme exemple dans discord.js API
 
-bot.on('guildMemberRemove', member => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'général');
-    // On veut envoyer un message d'au revoir dans un salon spécial par rapport à son nom pour qu'il s'adapte à tous les serveurs, si le channel n'existe pas, on arrête
-    if (!channel) return;
-    channel.send(`Bye ${member}`);
-    member.roles.add('membre');
-});
-
 bot.on('message', msg => {
+    if (msg.member.user.bot || message.channel.type === 'dm') return; //on n'accepte pas de communiquer entre bots, ni en message privé
     if (msg.content === 'ping') {
         msg.reply('Pong!');
+
     }
-}); //une fonction simple pour vérifier si le bot fonctionne
+}); //une fonction simple pour vérifier si le bot fonctionne, vient de l'api discord
 
 //Fonctions utilisées :
 
-
-
+//
 bot.login(token.token); //lance le bot
