745d8633193b99ad3b46590abb198232ea6dcc8f
Yvan-Haspekian <yvan.haspekian@etu.univ-amu.fr>
2021-03-15 09:54:02 +0100

    Ajout du système anti-spam pour commandes et messages

--- BrocoBot/events/message.js
+++ BrocoBot/events/message.js
@@ -2,46 +2,64 @@ module.exports =
 {
 	name: 'message',
 
-	execute(message, client, prefix, cooldowns, discord)
+	execute(message, client, prefix, discord)
 	{
-
-		if(!message.content.startsWith(prefix) || message.author.bot)
+		if(message.author.bot)
 			return;
-
-		const args = message.content.slice(prefix.length).trim().split(/ +/);
-		const commandName = args.shift().toLowerCase();
-
-		if (!client.commands.has(commandName))
-			return;
-
-		const command = client.commands.get(commandName);
-
-		if(!cooldowns.has(command.name))
-			cooldowns.set(command.name, new discord.Collection());
 
 		const now = Date.now();
-		const timestamps = cooldowns.get(command.name);
-		const cooldownAmount = (command.cooldown || 3) * 1000;
 
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime)
+		if(!message.content.startsWith(prefix))
 		{
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`Veuillez attendre ${timeLeft.toFixed(1)} seconde(s) avant de réutiliser cette commande.`);
+			const messageCooldown = 3000;
+			const messageCooldownExpiration = message.author.lastMessageTime + messageCooldown;
+
+			if (now > messageCooldownExpiration || Number.isNaN(messageCooldownExpiration))
+			{
+				message.author.lastMessageTime = now;
+			}
+			else
+			{
+				message.delete();
+				const timeLeft = (messageCooldownExpiration - now) / 1000;
+				return message.reply(`Veuillez attendre ${timeLeft.toFixed(1)} seconde(s) avant de renvoyer un message.`);
+			}
+
+			return;
 		}
-
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-		try
+		else
 		{
-			command.execute(message, args);
-		}
-		catch (error)
-		{
-			console.error(error);
-			message.reply('Erreur durant l\'exécution de la commande.');
+			const args = message.content.slice(prefix.length).trim().split(/ +/);
+			const commandName = args.shift().toLowerCase();
+
+			if (!client.commands.has(commandName))
+				return;
+
+			const command = client.commands.get(commandName);
+			
+			const commandCooldown = 3000;
+			const commandCooldownExpiration = message.author.lastCommandTime + commandCooldown;
+
+			if (now > commandCooldownExpiration || Number.isNaN(commandCooldownExpiration))
+			{
+				try
+				{
+					command.execute(message, args);
+				}
+				catch (error)
+				{
+					console.error(error);
+					message.reply('Erreur durant l\'exécution de la commande.');
+				}
+
+				message.author.lastCommandTime = now;
+			}
+			else
+			{
+				message.delete();
+				const timeLeft = (commandCooldownExpiration - now) / 1000;
+				return message.reply(`Veuillez attendre ${timeLeft.toFixed(1)} seconde(s) avant de réutiliser cette commande.`);
+			}
 		}
 	}
 };
--- BrocoBot/index.js
+++ BrocoBot/index.js
@@ -8,7 +8,6 @@ const client = new discord.Client();  //Client discord
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));		 //Fichiers d'évènements
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));	 //Fichiers de commandes
 
-const cooldowns = new discord.Collection();	 //Temps d'attente entre chaque exécution de commande
 client.commands = new discord.Collection();	 //Commandes
 
 for (const file of commandFiles)  //Lecture des commandes
@@ -24,7 +23,7 @@ for (const file of eventFiles)  //Lecture des évènements
 	if (event.once)
 		client.once(event.name, (...args) => event.execute(...args, client));
 	else
-		client.on(event.name, (...args) => event.execute(...args, client, prefix, cooldowns, discord));
+		client.on(event.name, (...args) => event.execute(...args, client, prefix, discord));
 }
 
 client.login(token);  //Connexion
