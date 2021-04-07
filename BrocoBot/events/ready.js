6fe4b82701e90e7c04e97145ed18582e952faef2
Yvan-Haspekian <yvan.haspekian@etu.univ-amu.fr>
2021-04-07 17:26:59 +0200

    Ajout de la lecture du fichier 'settings.ini'

--- BrocoBot/events/ready.js
+++ BrocoBot/events/ready.js
@@ -2,9 +2,30 @@ module.exports =
 {
 	name: 'ready',
 	once: true,
+	messageCooldown: 0,
+	commandCooldown: 0,
 
 	execute(client)
 	{
-		console.log(`Connecté en tant que ${client.user.tag}.`);
+		let fs = require('fs');
+
+		fs.readFile('settings/settings.ini', 'utf8' , (err, content) =>
+		{
+			if (err)
+			{
+			 	console.error(err);
+			    return;
+			}
+
+			let words = content.split(/\s|\n/);
+
+			messageCooldown = words[2];
+			commandCooldown = words[5];
+
+			module.exports.messageCooldown = messageCooldown;
+			module.exports.commandCooldown = commandCooldown;
+
+			console.log(`Connecté en tant que ${client.user.tag}.`);
+		})
 	}
 };
