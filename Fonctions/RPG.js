680b5b17a9b49c5fb184561a0f4986e1a0d740e2
InesHAMIDI <ineshamid@gmail.com>
2021-04-28 17:44:33 +0200

    I love SQL (no) I'm currently still working on turning json BD into sqlite

--- Fonctions/RPG.js
+++ Fonctions/RPG.js
@@ -1,11 +1,8 @@
 const Discord = require('discord.js'); //typique #include<>
+const bot = new Discord.Client(); //prérequis pour  créer le bot
 
 bot.on('message', msg => {
     if (msg.author.bot) return
 })
 
-function SaveBdPlayer() {
-    fs.writeFile("../BdPlayer.json", JSON.stringify(BdPlayer, null, 4), (err) => {
-        if (err) console.log("erreur d'accès à la BD player");
-    });
-} //on enregistre la BD quand on la modifie
+
