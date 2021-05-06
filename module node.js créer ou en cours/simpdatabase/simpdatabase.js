7e71c0bd7d8b5ecac5fc1e0870dd68a722afb542
taliesin-Aubert <73161306+taliesin-Aubert@users.noreply.github.com>
2021-05-06 12:39:07 +0200

    Update simpdatabase.js

--- module node.js créer ou en cours/simpdatabase/simpdatabase.js
+++ module node.js créer ou en cours/simpdatabase/simpdatabase.js
@@ -1,39 +1,65 @@
 const fs = require("fs");
 const path = require("path");
+const appDir = path.dirname(require.main.filename);
 
 const Create = name => {
     let nameFile = name + ".json";
-    const File = path.join(__dirname, nameFile);
+    const File = path.join(appDir, nameFile);
     try {
         fs.unlinkSync(File);
     } catch(e) {
     }
-    fs.writeFileSync(File, "");
+    fs.writeFileSync(File, "{}");
+    console.log("File created !")
 }
 
-const insert = (index, value, file) => {
+const Insert = (index, value, file) => {
     let out = fs.readFileSync(file);
     let data = JSON.parse(out.toString());
-    data[index]=value;
-    fs.writeFileSync(file, JSON.stringify(data));
+    if (data[index] == undefined) {
+        data[index]=value;
+        fs.writeFileSync(file, JSON.stringify(data));
+    } else {
+        console.log(index + ", Already exists !");
+    }    
 }
 
 const Delete = (index, file) => {
     let out = fs.readFileSync(file);
     let data = JSON.parse(out.toString());
-    data[index]=undefined;
-    fs.writeFileSync(file, JSON.stringify(data));
+    if (data[index] == undefined) {
+        console.log(index + ", does not exist !");
+    } else {
+        data[index]=undefined;
+        fs.writeFileSync(file, JSON.stringify(data));
+    } 
 }
 
-const search = (searchName, file) => {
+const Update = (index, newValue, file) => {
     let out = fs.readFileSync(file);
     let data = JSON.parse(out.toString());
-    return data[searchName];
+    if (data[index] == undefined) {
+        console.log(index + ", does not exist !");
+    } else {
+        data[index]=newValue;
+        fs.writeFileSync(file, JSON.stringify(data)); 
+    } 
+}
+
+const Search = (index, file) => {
+    let out = fs.readFileSync(file);
+    let data = JSON.parse(out.toString());
+    if (data[index] == undefined) {
+        console.log(index + ", does not exist !");
+    } else {
+        return data[index];
+    }
 }
 
 module.exports = {
     Create,
-    insert,
+    Insert,
     Delete,
-    search,
+    Update,
+    Search,
 }
