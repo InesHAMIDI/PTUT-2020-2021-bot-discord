acd7fca7894940b1f7b6e62885483ddf2ee5245b
Angèle Derrives <72963512+angele-DERRIVES@users.noreply.github.com>
2021-05-30 17:24:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -14,21 +14,28 @@ bot.on("ready", async () =>{
     bot.user.setActivity('rien parce qu on travaille hein');
 })
 
-
+//fonction qui envoie une question aléatoire
 bot.on('message', message => {
+    //commande pour déclancher la fonction
     if(message.content == '!question') {
 
+        //générer un nombre aléatoire
         const nombre = quiz[Math.floor(Math.random() * quiz.length)];
+        //on filtre la réponse pour savoir si elle correspond à celles de la liste 
         const filter = response => {
 	        return nombre.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
         };
 
+        //le bot envoie la question
         message.channel.send(nombre.question).then(() => {
+            //le bot attends les réponses des utilisateurs
 	        message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
 		        .then(collected => {
+                    //si la réponse est bonne 
 			        message.channel.send(`${collected.first().author} Bravo !`);
 		        })
 		        .catch(collected => {
+                    //si le temps est dépassé
 			        message.channel.send("Personne n'a trouvé... On retente ?");
 		        });
         });
