815790d2810811dc934e3daf6b4f09fa06b500be
Angèle Derrives <72963512+angele-DERRIVES@users.noreply.github.com>
2021-05-31 14:25:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const token = require("./token.json");
 const fs = require("fs");
 const bobot = require("./bobot.json");
 let request = require ('request');
-const quiz = require("./test.json")
+const quiz = require("./images.json")
+
 
 
 bot.on("ready", async () =>{
@@ -14,10 +15,11 @@ bot.on("ready", async () =>{
     bot.user.setActivity('rien parce qu on travaille hein');
 })
 
-//fonction qui envoie une question aléatoire
+
+//fonction qui envoie une image aléatoire
 bot.on('message', message => {
-    //commande pour déclancher la fonction
-    if(message.content == '!question') {
+    //commande pour déclencher la fonction
+    if(message.content == '!image') {
 
         //générer un nombre aléatoire
         const nombre = quiz[Math.floor(Math.random() * quiz.length)];
@@ -26,8 +28,9 @@ bot.on('message', message => {
 	        return nombre.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
         };
 
-        //le bot envoie la question
-        message.channel.send(nombre.question).then(() => {
+        //le bot envoie l'image
+        const image = { files: [`./images/${nombre.picture}`]};
+        message.channel.send(image).then(() => {
             //le bot attends les réponses des utilisateurs
 	        message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
 		        .then(collected => {
