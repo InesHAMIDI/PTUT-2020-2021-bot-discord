f06064c9eec97e585d42e16b42aaff6f9d6780c3
Angèle Derrives <72963512+angele-DERRIVES@users.noreply.github.com>
2021-06-08 17:07:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,39 @@ bot.on("ready", async () =>{
 })
 
 
-//fonction qui envoie une image aléatoire
+//fonction qui envoie une image aléatoire et analyse la réponse
+bot.on('message', message => {
+    //commande pour déclencher la fonction
+    if(message.content == '!image') {
+
+        //générer un nombre aléatoire
+        const nombre = quiz[Math.floor(Math.random() * quiz.length)];
+        //on filtre la réponse pour savoir si elle correspond à celles de la liste 
+        const filter = response => {
+	        return nombre.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+        };
+
+        //le bot envoie l'image
+        const image = { files: [`./images/${nombre.picture}`]};
+        const indice = (`${nombre.indice}`);
+        //message.channel.send(indice);
+        message.channel.send(image).then(() => {
+            //le bot attends les réponses des utilisateurs
+	        message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+		        .then(collected => {
+                    //si la réponse est bonne 
+			        message.channel.send(`${collected.first().author} Bravo ! C'était bien *${nombre.solution}*.`);
+		        })
+		        .catch(collected => {
+                    //si le temps est dépassé
+			        message.channel.send(`Personne n'a trouvé... C'était *${nombre.solution}*. On retente ?`);
+		        });
+        });
+    }
+});
+
+/*
+//fonction qui envoie une image aléatoire et analyse la réponse
 bot.on('message', message => {
     //commande pour déclencher la fonction
     if(message.content == '!image') {
@@ -35,15 +67,15 @@ bot.on('message', message => {
 	        message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
 		        .then(collected => {
                     //si la réponse est bonne 
-			        message.channel.send(`${collected.first().author} Bravo !`);
+			        message.channel.send(`${collected.first().author} Bravo ! C'était bien ${nombre.solution}`);
 		        })
 		        .catch(collected => {
                     //si le temps est dépassé
-			        message.channel.send("Personne n'a trouvé... On retente ?");
+			        message.channel.send(`Personne n'a trouvé... C'était ${nombre.solution}. On retente ?`);
 		        });
         });
     }
 });
-
+*/
 
 bot.login(token.token);
